663489d15d12c7f8f16c8df0cbe1068639a696f9
Kankai Zhang <zkk_zero@hotmail.com>
2016-09-29 15:40:00 +1000

    add test case for direct matching logic

--- main.go
+++ main.go
@@ -18,27 +18,32 @@ type QueryOption struct {
 
 // QueryResult ...
 type QueryResult struct {
-	Count int64 `json:"count"`
+	Count          int64    `json:"count"`
+	Filterfeatures []string `json:"filterfeatures"`
 }
+
+var (
+	baseURL = "http://localhost:8000"
+	query   string
+	opt     QueryOption
+	result  QueryResult
+)
 
 func getJSON(url string) QueryResult {
 	r, err := http.Get(url)
 	if err != nil {
-		// return err
 		panic(err.Error())
 	}
 	defer r.Body.Close()
 	var message QueryResult
-	if r.StatusCode == 200 { // OK
-		// bodyBytes, _ := ioutil.ReadAll(r.Body)
+	if r.StatusCode == 200 {
 		json.NewDecoder(r.Body).Decode(&message)
-		// fmt.Printf("%v", message["hasProfile"])
 	}
 	return message
 }
 
 func fetchResult(query string, option QueryOption) QueryResult {
-	url := fmt.Sprintf("http://localhost:8000/melbourne/api/search/%s?format=json", query)
+	url := fmt.Sprintf("%s/melbourne/api/search/%s?format=json", baseURL, query)
 	opt, _ := json.Marshal(option)
 	if len(string(opt)) > 0 {
 		url = url + "&o=" + string(opt)
@@ -49,15 +54,9 @@ func fetchResult(query string, option QueryOption) QueryResult {
 }
 
 func testSth() {
-	var (
-		query  string
-		opt    QueryOption
-		result QueryResult
-	)
 	opt = QueryOption{
 		Features: []string{"takeaway"}}
 	result = fetchResult(query, opt)
-	// count = result["count"] //type assertion convert interface{} into int
 	if result.Count < 20 {
 		fmt.Printf("count should be greater than 20 while it is %v", result.Count)
 	} else {
@@ -65,7 +64,21 @@ func testSth() {
 	}
 }
 
+// testDirectMatch matches venue name "hinoki japanese pantry"
+// to prevent search enginee parsing name as "hinoki pantry" + feature "Japanese"
+func testDirectMatch() {
+	fmt.Println("Testing direct matching logic")
+	query = "hinoki japanese pantry"
+	result = fetchResult(query, opt)
+	if len(result.Filterfeatures) > 0 {
+		fmt.Println("Should not parse a direct match")
+	} else {
+		fmt.Printf("Pass")
+	}
+}
+
 func main() {
-	testSth()
+	//testSth()
+	testDirectMatch()
 
 }
